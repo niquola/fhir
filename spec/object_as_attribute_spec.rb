6e5dfe4eba5aff6f9d16a5056152bcc808c2dcfc
nicola <niquola@gmail.com>
2013-09-09 19:01:50 +0400

    broken spec for data types

---
+++ spec/object_as_attribute_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe 'AllergyIntolerance' do
+  example do
+    bad_gender = Fhir::Coding.new(system: 'local', code: 'G', display: 'germofrodit')
+    pt = Fhir::Patient.new({gender: bad_gender})
+  end
+end
