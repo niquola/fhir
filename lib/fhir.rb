18606659e59b07ff3e05f0fa6594d24f51d36f16
Sasha Alexandrov <elequtree@gamil.com>
2013-09-16 16:56:32 +0400

    Fixed load_handmade.

--- lib/fhir.rb
+++ lib/fhir.rb
@@ -25,6 +25,8 @@ require "fhir/models/autoloads"
 require "fhir/types/autoloads"
 
 def Fhir.load_handmade(file_name)
-  fname = File.dirname(__FILE__) + "fhir/handmade/#{file_name}"
-  load(fname) if File.exists?(fname)
+  fname = File.dirname(__FILE__) + "/fhir/handmade/#{file_name}.rb"
+  if File.exists?(fname)
+    load(fname)
+  end
 end
